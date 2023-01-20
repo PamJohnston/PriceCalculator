e54cedc9c20d41f192512167279c7d3203212a07
PamJohnston <p.johnston2@rgu.ac.uk>
2023-01-20 13:48:32 +0000

    Added price calculating functionality to api on backend

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,25 @@ var options = {
 var dir = path.join(__dirname, '../frontend');
 
 app.get('/api', function(req, res){
-    res.send("Hello world!")
+    res.send("Yes we have an API now")
+});
 
+// e.g. test using:
+//http://127.0.0.1:8000/api/getPrice?salary=2000&days=20
+app.get('/api/getPrice', function(req, res){
+    //res.send("Hello world!")
+    // Copied from front end
+    var s = req.query.salary;
+    var d = req.query.days;
+    console.log("Calculating price")
+    console.log(s)
+    console.log(d)
+    let finalPrice = 0;
+    dailyRate = s/365;
+    price = Math.round(dailyRate * d);
+    var roundToNearest = 50;
+    roundedPrice = Math.round((price+roundToNearest)/roundToNearest) * roundToNearest // Always round up
+    res.send(""+roundedPrice)
 });
 
 app.use(express.static(dir, options));
